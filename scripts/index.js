bd13841f7d2edab8ef4da8c2d947e5fde00a5b77
Santiago Gutierrez <santiagods004@gmail.com>
2024-04-25 19:20:04 -0500

    Función js implementada

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+const popupProfile = document.querySelector(".popup")
+const btnEdit = document.querySelector(".profile__icon-edit");
+const btnClose = document.querySelector(".popup__icon");
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__description");
+const inputName = document.querySelector(".popup__input-name");
+const inputJob = document.querySelector(".popup__input-job");
+const btnSave = document.querySelector(".popup__btn");
+
+
+
+function openPopup(){
+  inputsReset();
+  popupProfile.classList.add("popup__show");
+}
+
+btnEdit.addEventListener("click", openPopup);
+
+function closePopup(){
+  popupProfile.classList.remove("popup__show");
+}
+
+btnClose.addEventListener("click", closePopup);
+
+function inputsReset(){
+  inputName.value = "";
+  inputJob.value = "";
+}
+
+function saveInfo(evt){
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  closePopup();
+}
+
+btnSave.addEventListener("click", saveInfo);
