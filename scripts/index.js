7a6c7961b153bcab48d17495e70047177964af69
Santiago Gutierrez <santiagods004@gmail.com>
2024-05-13 14:01:59 -0500

    Cargar tarjetas por js

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,64 @@
-const popupProfile = document.querySelector(".popup")
+//Variables user
+const btnAdd = document.querySelector(".profile__btn-add")
 const btnEdit = document.querySelector(".profile__icon-edit");
-const btnClose = document.querySelector(".popup__icon");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__description");
-const inputName = document.querySelector(".popup__input-name");
-const inputJob = document.querySelector(".popup__input-job");
-const btnSave = document.querySelector(".popup__btn");
-const btnLike = document.querySelectorAll(".elements__icon-like");
+//Variables popupU
+const popupProfile = document.querySelector(".popupU");
+const btnCloseU = document.querySelector(".popupU__icon");
+const inputName = document.querySelector(".popupU__input-name");
+const inputJob = document.querySelector(".popupU__input-job");
+const btnSave = document.querySelector(".popupU__btn");
+//Variables PopupS
+const popupSite = document.querySelector(".popupS");
+const btnCloseS = document.querySelector(".popupS__icon");
+//Variables elements cards
+const itemElements = document.querySelector(".elements");
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+  }
+];
+const template = document.querySelector(".template-card");
 
-btnEdit.addEventListener("click", openPopup);
-btnClose.addEventListener("click", closePopup);
-btnSave.addEventListener("click", saveInfo);
+//Botones popupU
+btnEdit.addEventListener("click", openPopupU);
+btnCloseU.addEventListener("click", closePopupU);
+btnSave.addEventListener("click", saveInfoU);
+//Botones popupS
+btnAdd.addEventListener("click", openPopupS);
+btnCloseS.addEventListener("click", closePopupS);
 
-function openPopup(){
+
+//Funcionamiento popupU
+function openPopupU(){
   inputsShowInfo();
-  popupProfile.classList.add("popup__show");
+  popupProfile.classList.add("popupU__show");
 }
 
-function closePopup(){
-  popupProfile.classList.remove("popup__show");
+function closePopupU(){
+  popupProfile.classList.remove("popupU__show");
 }
 
 function inputsShowInfo(){
@@ -26,13 +66,33 @@ function inputsShowInfo(){
   inputJob.value = profileJob.textContent;
 }
 
-function saveInfo(evt){
+function saveInfoU(evt){
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
-  closePopup();
+  closePopupU();
+}
+//Funcionamiento popupS
+function openPopupS(){
+  popupSite.classList.add("popupS__show");
 }
 
+function closePopupS(){
+  popupSite.classList.remove("popupS__show");
+}
+//Funcionamiento elements
+function createCards(name, link){
+  const card = template.cloneNode(true).content.querySelector(".elements__target");
+  const cardImage = card.querySelector(".elements__image");
+  const cardTitle = card.querySelector(".elements__txt");
+  cardImage.src = link;
+  cardImage.alr = name;
+  cardTitle.textContent = name;
+  itemElements.append(card);
+}
 
+initialCards.forEach(function (elem) {
+  createCards(elem.name, elem.link);
+})
 
 
